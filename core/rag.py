54361d709a76f5d7f747d428faedd545b2991014
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2026-03-05 15:55:34 +0530

    add persistent FAISS index

--- core/rag.py
+++ core/rag.py
@@ -7,6 +7,7 @@ from langchain_community.vectorstores import FAISS
 
 # load env variables
 load_dotenv()
+INDEX_PATH = "vector_index"
 
 api_key = os.getenv("OPENROUTER_API_KEY")
 
@@ -15,18 +16,32 @@ def build_vector_store(path: str):
     if api_key is None:
         raise ValueError("OPENROUTER_API_KEY not set")
 
-    with open(path) as f:
-        chunks = json.load(f)
-
-    texts = [c["content"] for c in chunks]
-
     embeddings = OpenAIEmbeddings(
         model="text-embedding-3-small",  # embedding model
         base_url="https://openrouter.ai/api/v1",
         api_key=SecretStr(api_key),
     )
 
+    # load existing index if present
+    if os.path.exists(INDEX_PATH):
+        print("[RAG] Loading existing FAISS index...")
+        return FAISS.load_local(
+            INDEX_PATH, embeddings, allow_dangerous_deserialization=True
+        )
+
+    print("[RAG] Building FAISS index...")
+
+    with open(path) as f:
+        chunks = json.load(f)
+
+    texts = [c["content"] for c in chunks]
+
     vectorstore = FAISS.from_texts(texts, embeddings)
+
+    vectorstore.save_local(INDEX_PATH)
+
+    print("[RAG] Index saved to disk.")
+
     return vectorstore
 
 
